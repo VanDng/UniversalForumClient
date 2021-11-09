1ea70458d1f37e3891665437da34d3771afa8343
Dustin <dustin.dang@websparks.sg>
2021-11-09 18:15:35 +0700

    implement

--- Src/UriManager.cs
+++ Src/UriManager.cs
@@ -13,13 +13,21 @@ namespace UniversalForumClient.Core
         private static string ThreadNamePlaceHolder = "_ThreadName_";
         private static string ThreadPagePlaceHolder = "_ThreadName_";
 
+        private static string Home = $"{string.Empty}";
         private static string Forum = $"forums/{ForumNamePlaceHolder}/page-{ForumPagePlaceHolder}";
         private static string Thread = $"threads/{ForumNamePlaceHolder}/page-{ForumPagePlaceHolder}";
 
         public static string ForumUri(string forumName, int forumPage)
         {
-            return Forum.Replace(ForumNamePlaceHolder, forumName)
-                        .Replace(ForumPagePlaceHolder, forumPage.ToString());
+            if (string.IsNullOrEmpty(forumName))
+            {
+                return Home;
+            }
+            else
+            {
+                return Forum.Replace(ForumNamePlaceHolder, forumName)
+                            .Replace(ForumPagePlaceHolder, forumPage.ToString());
+            }
         }
 
         public static string ThreadUri(string threadName, int threadPage)
