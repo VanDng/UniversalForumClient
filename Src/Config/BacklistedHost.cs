7cbcc92083854be43a95cb42e7ea633b47ae1905
Van <20492454+VanDng@users.noreply.github.com>
2021-12-11 22:31:04 +0700

    impl

---
+++ Src/Config/BacklistedHost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversalForumClient.Config
+{
+    public static class BacklistedHost
+    {
+        public static IEnumerable<string> Items()
+        {
+            return new string[]
+            {
+                "l.yimg.com"
+            };
+        }
+    }
+}
